62c67608803ea0484add4a844bae9a2c8866dc3e
ikura-hamu <harukikainuma@gmail.com>
2022-07-13 15:23:01 +0900

    トップカードの設定

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-import Calculater from './components/Calculater.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Calculater from './components/Calculater.vue';
 </script>
 
 <template>
